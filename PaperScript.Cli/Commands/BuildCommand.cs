a3d490271a205b35c80828b985e7ea32725fd836
Jakub Sycha <jsycha@jakubsycha.com>
2025-06-08 23:41:35 +0200

    fix output path

--- PaperScript.Cli/Commands/BuildCommand.cs
+++ PaperScript.Cli/Commands/BuildCommand.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Diagnostics;
 using GlobExpressions;
 using PaperScript.Cli.Config;
@@ -11,7 +12,13 @@ namespace PaperScript.Cli.Commands;
 
 public class BuildCommand : Command<BuildCommand.BuildCommandSettings>
 {
-    public class BuildCommandSettings : CommandSettings {}
+    public class BuildCommandSettings : CommandSettings
+    {
+        [CommandOption("-n|--no-compile")]
+        [Description("do not run the papyrus compiler")]
+        [DefaultValue(false)]
+        public bool DoNotCompile { get; set; }
+    }
 
 
     public override int Execute(CommandContext context, BuildCommandSettings settings)
@@ -45,13 +52,13 @@ public class BuildCommand : Command<BuildCommand.BuildCommandSettings>
             return 1;
         }
 
-        if (!File.Exists(manifest.PapyrusFlagsPath))
+        if (!File.Exists(manifest.PapyrusFlagsPath) && !settings.DoNotCompile)
         {
             Log.Error("the papyrus flags file does not exist");
             return 1;
         }
 
-        if (!File.Exists(manifest.PapyrusCompilerPath))
+        if (!File.Exists(manifest.PapyrusCompilerPath) && !settings.DoNotCompile)
         {
             Log.Error("the papyrus compiler executable does not exist");
             return 1;
@@ -69,51 +76,55 @@ public class BuildCommand : Command<BuildCommand.BuildCommandSettings>
 
         foreach (var file in files)
         {
+            
             Log.Debug("transpiling {In}", file);
             var code = File.ReadAllText(file);
             var result = tp.Transpile(code);
-            var outputPath = Path.Combine(manifest.ScriptOutputPath, Path.ChangeExtension(file, ".psc"));
+            var outputPath = Path.Combine(manifest.ScriptFolderPath, Path.ChangeExtension(Path.GetFileName(file), ".psc"));
             File.WriteAllText(outputPath, result.Code);
             Log.Debug("transpiled file {In} -> {Out}", file, outputPath);
-            
-            Log.Debug("compiling papyrus script {In}", outputPath);
-            
-            var compiler = new Process
-            { 
-                StartInfo =
-                { 
-                    FileName = manifest.PapyrusCompilerPath,
-                    ArgumentList =
-                    {
-                        outputPath,
-                        $"-i=\"{manifest.ScriptFolderPath}\"",
-                        $"-o=\"{manifest.ScriptOutputPath}\"",
-                        $"-f=\"{manifest.PapyrusFlagsPath}\"",
-                        "-q"
-                    },
-                    RedirectStandardError = true,
-                    RedirectStandardInput = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    Verb = null
-                }
-            };
 
-            compiler.Start();
-
-            var output = compiler.StandardOutput.ReadToEnd();
-            var error = compiler.StandardError.ReadToEnd();
-            
-            output.Split('\n').ToList().ForEach(line => Log.Debug("[Papyrus Compiler] {Line}", line));
-            
-
-            compiler.WaitForExit();
-
-            if (compiler.ExitCode != 0)
+            if (!settings.DoNotCompile)
             {
-                Log.Error("compilation failed:");
-                error.Split('\n').ToList().ForEach(line => Log.Error("[Papyrus Compiler] {Line}", line));
+                Log.Debug("compiling papyrus script {In}", outputPath);
+            
+                var compiler = new Process
+                { 
+                    StartInfo =
+                    { 
+                        FileName = manifest.PapyrusCompilerPath,
+                        ArgumentList =
+                        {
+                            outputPath,
+                            $"-i=\"{manifest.ScriptFolderPath}\"",
+                            $"-o=\"{manifest.ScriptOutputPath}\"",
+                            $"-f=\"{manifest.PapyrusFlagsPath}\"",
+                            "-q"
+                        },
+                        RedirectStandardError = true,
+                        RedirectStandardInput = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        Verb = null
+                    }
+                };
+
+                compiler.Start();
+
+                var output = compiler.StandardOutput.ReadToEnd();
+                var error = compiler.StandardError.ReadToEnd();
+            
+                output.Split('\n').ToList().ForEach(line => Log.Debug("[Papyrus Compiler] {Line}", line));
+            
+
+                compiler.WaitForExit();
+
+                if (compiler.ExitCode != 0)
+                {
+                    Log.Error("compilation failed:");
+                    error.Split('\n').ToList().ForEach(line => Log.Error("[Papyrus Compiler] {Line}", line));
+                }   
             }
         }
         
