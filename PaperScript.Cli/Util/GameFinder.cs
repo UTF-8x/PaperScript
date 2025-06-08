0ff122bffed8696da0e40d9fa4abf6cc92038f6a
Jakub Sycha <jsycha@jakubsycha.com>
2025-06-08 23:42:37 +0200

    fix typo

--- PaperScript.Cli/Util/GameFinder.cs
+++ PaperScript.Cli/Util/GameFinder.cs
@@ -5,7 +5,7 @@ namespace PaperScript.Cli.Util;
 public class GameFinder
 {
 #if WINDOWS
-    public static string? GetSkyrimSEInstallDir() {
+    public static string? GetSkyrimSeInstallDir() {
         var registryPath = @"Computer\HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Bethesda Softworks\Skyrim Special Edition";
 
         using var key = Registry.LocalMachine.OpenSubKey(registryPath);
